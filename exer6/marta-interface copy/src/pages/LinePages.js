7f2a30ebcc32c010679d0762ecbd4aed9670a910
nikvijay07 <nikvijay07@gmail.com>
2023-10-19 18:47:46 -0400

    Adding buttons and introducint first useEffect case for data

--- exer6/marta-interface copy/src/pages/LinePages.js
+++ exer6/marta-interface copy/src/pages/LinePages.js
@@ -3,17 +3,26 @@ import stationData from '../server/stationData';
 import trainData from '../server/trainData';
 import TrainList from '../components/TrainList';
 import NavBar from '../components/NavBar';
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import LineSelector from '../components/LineSelector';
+
+const API_URL = "http://13.59.196.129:3001/arrivals/";
 
 export default function LinesPage() {
-    const[currColor, setColor] = useState("Gold");
+    const[currColor, setColor] = useState("gold");
     const [currStation, setStation] = useState("Chamblee");
-    
-    
+    const [data, setData] = useState(null);
 
+    useEffect(() => {
+      fetch(API_URL + currColor)
+      .then(response => response.json())
+      .then(data => setData(data))
+    },[])
+    
 
     return (
     <div id = "everything">
+      <LineSelector></LineSelector>
       <div class = "trainColorHeading">
           {/* <img src ={martaLogo} alt = "Marta logo"/> */}
           <h1 class = "heading" style = {{fontWeight: 'bold'}}> {currColor} </h1>
